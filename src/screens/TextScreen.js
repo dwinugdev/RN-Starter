783e037cab2f610817c232ec377aa152d93ea843
dwiNugroho <dwinug.dev@gmail.com>
2026-07-11 19:40:30 +0700

    Section 6 - 60. Handling Text Input

---
+++ src/screens/TextScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const TextScreen = () => {
+  return (
+    <View>
+      <Text>TextScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default TextScreen;
