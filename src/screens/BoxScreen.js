b7299e62daa9c3d23485df3d10fb2cbe2a5023f9
dwiNugroho <dwinug.dev@gmail.com>
2026-07-14 18:47:56 +0700

    Section 6 - 72. Flex Values

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,21 +4,30 @@ import React from 'react';
 const BoxScreen = () => {
   return (
     <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>Box Screen</Text>
+      <Text style={styles.textOneStyle}>Child #1</Text>
+      <Text style={styles.textTwoStyle}>Child #2</Text>
+      <Text style={styles.textThreeStyle}>Child #3</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   viewStyle: {
-    borderWidth: 1,
+    borderWidth: 3,
     borderColor: 'black',
+    height: 200,
   },
-  textStyle: {
-    borderWidth: 1,
+  textOneStyle: {
+    borderWidth: 3,
     borderColor: 'red',
-    marginVertical: 10,
-    marginHorizontal: 10,
+  },
+  textTwoStyle: {
+    borderWidth: 3,
+    borderColor: 'green',
+  },
+  textThreeStyle: {
+    borderWidth: 3,
+    borderColor: 'blue',
   },
 });
 
