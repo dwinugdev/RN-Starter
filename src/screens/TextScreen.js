c5bc2c62178090e9353ad60e31fd0b9d806c86bb
dwiNugroho <dwinug.dev@gmail.com>
2026-07-14 08:39:58 +0700

    Section 6 - 63. Weird Thins with Text and State

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,14 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import React from "react";
 
 const TextScreen = () => {
   return (
     <View>
-      <Text>TextScreen</Text>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value="Hi there"
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    margin: 15,
+    borderColor: "black",
+    borderWidth: 1,
+  },
+});
 
 export default TextScreen;
